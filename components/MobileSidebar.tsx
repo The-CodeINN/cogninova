200b19a5a626b7ca8d63d5aa6465afb866674957
Richard Emijere <emijere.richard@gmail.com>
2023-07-22 14:45:57 +0000

    Feat: highlight current path

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -5,8 +5,19 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./Sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -14,7 +25,7 @@ const MobileSidebar = () => {
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="p-0">
+      <SheetContent side='left' className='p-0'>
         <Sidebar />
       </SheetContent>
     </Sheet>
